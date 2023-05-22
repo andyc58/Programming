d089df16cdf8617b40a721dc5249f0077662c7b5
andyc58 <73554950+andyc58@users.noreply.github.com>
2023-05-22 16:13:01 -0400

    Update app.py

--- Dash Stock App/app.py
+++ Dash Stock App/app.py
@@ -9,8 +9,6 @@ from yfinance import Ticker
 from dash.exceptions import PreventUpdate
 import re
 
-year_now = datetime.now().year
-start, end = datetime(year_now, 1, 1), datetime.now()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Stocks App'
